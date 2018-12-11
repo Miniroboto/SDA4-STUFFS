b7142f20b8b54f83e08441603586eb7c3053998c
Miniroboto <44720947+Miniroboto@users.noreply.github.com>
2018-12-11 19:17:24 +0200

    Add files via upload

---
+++ LinkedList1/lista/src/Main.java
@@ -0,0 +1,22 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Lista<Integer>listOfIntegers = new Lista<>();
+        Lista<String>listOfStings = new Lista<>();
+        listOfIntegers.add(4);
+        listOfIntegers.add(2);
+        System.out.println(listOfIntegers.toString());
+
+
+//        Lista listaTest = new Lista();
+//        listaTest.add("ceva");
+//        listaTest.add("ceva2");
+//        listaTest.add("ceva3");
+//        System.out.println(listaTest.toString());
+//        listaTest.remove(0);
+//        System.out.println(listaTest.toString());
+//        System.out.println(listaTest.get(1));
+
+    }
+}
